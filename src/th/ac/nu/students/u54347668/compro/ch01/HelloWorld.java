5417a3d76563baddebcccc4e088c148029a21332
fatoeeeyfa <fatoeeeyfa@hotmail.com>
2012-06-19 16:12:03 +0700

    เพิ่ม "Programming is fun"

--- src/th/ac/nu/students/u54347668/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54347668/compro/ch01/HelloWorld.java
@@ -3,6 +3,6 @@ package th.ac.nu.students.u54347668.compro.ch01;
 public class HelloWorld {
 public static void main (String[]args){
    System.out.println("Hello,World");
-
+   System.out.println("Programming is fun");
 }
 }
