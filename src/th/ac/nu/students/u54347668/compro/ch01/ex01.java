e8668dfec45845adc258144b8144e3ccbcb6d286
fatoeeeyfa <fatoeeeyfa@hotmail.com>
2012-07-10 17:03:14 +0700

    Chapters2

---
+++ src/th/ac/nu/students/u54347668/compro/ch01/ex01.java
@@ -0,0 +1,13 @@
+
+package th.ac.nu.students.u54347668.compro.ch01;
+
+public class ex01{
+    public static void main(String[]args){
+   int num1 = 2147483647;
+   System.out.println(num1);   // 2147483647
+   System.out.println(num1+1);   // -2147483648
+   int num2 = -2147483648;
+   System.out.println(num2);   //-2147483648
+   System.out.println(num2-1);   // 2147483647
+    }
+  }
